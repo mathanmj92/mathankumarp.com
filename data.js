091f3e3da5666506c843d44ba2f9ca3f912eeeef
mathanmj92 <mathannits92@gmail.com>
2025-08-13 16:31:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,66 @@
+// Portfolio Data - This is the only file you need to update
+const portfolioData = {
+  "basicInfo": {
+    "name": "Dr. P. Mathan Kumar",
+    "designation": "Post Doctoral Researcher",
+    "slogan": "\"Passionate about research on thin film fabrication and nanomaterial synthesis \\nfor electrochemical applications\"",
+    "profileImage": "https://scontent-maa3-1.xx.fbcdn.net/v/t1.6435-9/124442213_3420429658053699_3691824020500810128_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=eEdnA4dPM30Q7kNvwEDHsYt&_nc_oc=AdmSn4uCs_BxnmY6mz5RxfTUMvccUENwh_ObqJMhG8tnWyIaOGXahe-8MTVl9DS-UQ8&_nc_zt=23&_nc_ht=scontent-maa3-1.xx&_nc_gid=7hFA-ONVOKeK4AAJKK84EQ&oh=00_AfWmEp5clWzPbTwojM1aaQxlWBHIB6PkhAPhb8iMqFha0w&oe=68C28D22",
+    "cvLink": "#"
+  },
+  "stats": {
+    "publications": "42",
+    "citations": "1,280",
+    "hIndex": "24",
+    "iIndex": "38"
+  },
+  "contact": {
+    "email": "dr.jane.smith@research.edu",
+    "location": "Institute of Advanced Sciences\u003cbr\u003eCambridge, MA 02139"
+  },
+  "footer": {
+    "title": "Senior Research Scientist | Quantum Computing Researcher",
+    "description": "Dedicated to advancing quantum technologies through innovative research and collaboration"
+  },
+  "timeline": [
+    {
+      "date": "2022 - Present",
+      "title": "Senior Research Scientist",
+      "description": "Leading research in quantum computing applications at the Institute of Advanced Sciences. Supervising PhD candidates and managing research projects."
+    },
+    {
+      "date": "2019 - 2022",
+      "title": "Postdoctoral Researcher",
+      "description": "Conducted groundbreaking research in quantum algorithms at MIT Quantum Computing Lab under Dr. Michael Chen."
+    },
+    {
+      "date": "2015 - 2019",
+      "title": "Ph.D. in Quantum Physics",
+      "description": "Stanford University. Thesis: 'Quantum Entanglement in Superconducting Circuits'. Advisor: Dr. Michael Chen."
+    },
+    {
+      "date": "2013 - 2015",
+      "title": "M.Sc. in Physics",
+      "description": "University of Cambridge. Specialized in Quantum Mechanics and Statistical Physics. Graduated with distinction."
+    },
+    {
+      "date": "2009 - 2013",
+      "title": "B.Sc. in Physics",
+      "description": "California Institute of Technology. Graduated with honors. Minor in Computer Science."
+    }
+  ],
+  "publications": {
+    "journal": [],
+    "conference": [],
+    "chapter": [],
+    "patent": []
+  },
+  "researchCircle": [],
+  "gallery": [],
+  "collections": {
+    "videos": [],
+    "links": [],
+    "documents": []
+  },
+  "updates": [],
+  "socialLinks": []
+};
