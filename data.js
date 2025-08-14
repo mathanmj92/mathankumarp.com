b1ab2ee41b6be7a1461fe0b126af1e85ae71cceb
mathanmj92 <mathannits92@gmail.com>
2025-08-14 11:53:53 +0530

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const portfolioData = {
     "designation": "Post Doctoral Researcher",
     "slogan": "\"Passionate about research on thin film fabrication and nanomaterial synthesis \\nfor electrochemical applications\"",
     "profileImage": "https://scontent-maa3-1.xx.fbcdn.net/v/t1.6435-9/124442213_3420429658053699_3691824020500810128_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=eEdnA4dPM30Q7kNvwEDHsYt&_nc_oc=AdmSn4uCs_BxnmY6mz5RxfTUMvccUENwh_ObqJMhG8tnWyIaOGXahe-8MTVl9DS-UQ8&_nc_zt=23&_nc_ht=scontent-maa3-1.xx&_nc_gid=7hFA-ONVOKeK4AAJKK84EQ&oh=00_AfWmEp5clWzPbTwojM1aaQxlWBHIB6PkhAPhb8iMqFha0w&oe=68C28D22",
-    "cvLink": "#"
+    "cvLink": "https://drive.google.com/file/d/1DNbmklnHMQou7ZrfhYWVGVhO8509hZuz/view?usp=sharing"
   },
   "stats": {
     "publications": "16",
@@ -54,9 +54,201 @@ const portfolioData = {
     }
   ],
   "publications": {
-    "journal": [],
-    "conference": [],
-    "chapter": [],
+    "journal": [
+      {
+        "id": 1,
+        "title": "Enhancing surface area, morphology, and electrocatalytic activity favouring triiodide reduction via surface  sulfurization  of  metal  oxide",
+        "authors": "P, Mathan Kumar R, Sharan Peri, Rajagopal T, Mohana Selvi V, Mareeswaran S, Brindha B, Muthuraaman",
+        "journal": "Electrochimica Acta",
+        "year": "2025",
+        "description": "",
+        "doi": "10.1016/j.electacta.2023.143616"
+      },
+      {
+        "id": 2,
+        "title": "Unveiling the role of Cr-doped biphasic TiO2 photoanode for improving the photovoltaic performance of dye-sensitized solar cel",
+        "authors": "S.K, Nikhil Kumar, P. Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G.",
+        "journal": "Applied Physics A: Materials Science and Processing",
+        "year": "2025",
+        "description": "",
+        "doi": "10.1007/s00339-024-08190-3"
+      },
+      {
+        "id": 3,
+        "title": "A doped heterojunction photoanode: a strategy-driven approach to improve the charge carrier dynamics and photoconversion performance of the TiO2-based dye-sensitized solar cell",
+        "authors": "Nikhil, S. K. Nair, Gopika Rajeev Kumar, Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Journal of Materials Science: Materials in Electronics",
+        "year": "2025",
+        "description": "",
+        "doi": "10.1007/s10854-024-12344-7"
+      },
+      {
+        "id": 4,
+        "title": "Multiphasic titania photoanode: A facile phase assisted approach to enhance the photoelectrochemical and photovoltaic performance",
+        "authors": "Ningthoukhongjam, Pujita Mathan Kumar, P. Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Optical Materials",
+        "year": "2024",
+        "description": "",
+        "doi": "10.1016/j.optmat.2024.115995"
+      },
+      {
+        "id": 5,
+        "title": "Ag-modified ZnO nanorods and its dual application in visible light-driven photoelectrochemical water oxidation and photocatalytic dye degradation: A correlation between optical and electrochemical properties",
+        "authors": "Das, Abinash Deka, Trinayana Kumar, P. Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G.",
+        "journal": "Advanced Powder Technology",
+        "year": "2022",
+        "description": "",
+        "doi": "10.1016/j.apt.2022.103434"
+      },
+      {
+        "id": 6,
+        "title": "Enhancement of photoelectric properties of Cu2ZnSnS4 thin films by electronic excitations induced by swift heavy ions",
+        "authors": "Sampath, M. Logu, T. Mathan Kumar, P. Asokan, K. Sethuraman, K",
+        "journal": "Materials Science and Engineering: B",
+        "year": "2022",
+        "description": "",
+        "doi": "10.1016/j.mseb.2022.115683"
+      },
+      {
+        "id": 7,
+        "title": "Effect of anatase-rutile phase ratio of titania photoanode on photoelectrochemical performance and photoconversion efficiency",
+        "authors": "\"Ningthoukhongjam, Pujita K, Nikhil S. Kumar P, Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G. ",
+        "journal": "Optical Materials",
+        "year": "2022",
+        "description": "",
+        "doi": "10.1016/j.optmat.2022.112269"
+      },
+      {
+        "id": 8,
+        "title": "Bio-engineered Hexagon-shaped Co3O4 Nanoplates on Deoxyribonucleic Acid (DNA) Scaffold: An Efficient Electrode Material for an Asymmetric Supercapacitor and Electrocatalysis Application",
+        "authors": "Yesuraj, Johnbosco Lee, Hye-One Pandiyan, Mathan kumar Jayavelu, Jayachandiran Bhagavathiachari, Muthuraaman Kim, Kibum",
+        "journal": "Journal of Molecular Structure",
+        "year": "2022",
+        "description": "",
+        "doi": "10.1016/j.molstruc.2022.132499"
+      },
+      {
+        "id": 9,
+        "title": "Zero- and One-Dimensional Lead-Free Perovskites for Photoelectrochemical Applications",
+        "authors": "Anthony, Ancy Milrad Pandian, Mathan Kumar Pandurangan, Prabhu Bhagavathiachari, Muthuraaman",
+        "journal": "ACS Applied Materials and Interfaces",
+        "year": "2025",
+        "description": "",
+        "doi": "10.1021/acsami.2c04407"
+      },
+      {
+        "id": 10,
+        "title": "Role of type II heterojunction in ZnO–In2O3 nanodiscs for enhanced visible-light photocatalysis through the synergy of effective charge carrier separation and charge transport",
+        "authors": "Das, Abinash Patra, Moumita Kumar P, Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Materials Chemistry and Physics",
+        "year": "2021",
+        "description": "",
+        "doi": "10.1016/j.matchemphys.2021.124431"
+      },
+      {
+        "id": 11,
+        "title": "Shape selective flower-like ZnO nanostructures prepared via structure-directing reagent free methods for efficient photocatalytic performance",
+        "authors": "Das, Abinash Mathan Kumar, P. Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Materials Science and Engineering B: Solid-State Materials for Advanced Technology",
+        "year": "2021",
+        "description": "",
+        "doi": "10.1016/j.mseb.2021.115149"
+      },
+      {
+        "id": 12,
+        "title": "Effect of aspect ratio of c-axis oriented ZnO nanorods on photoelectrochemical performance and photoconversion efficiency",
+        "authors": "S K, Nikhil Das, Abinash Kumar P, Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Optical Materials",
+        "year": "2021",
+        "description": "",
+        "doi": "10.1016/j.optmat.2021.111551"
+      },
+      {
+        "id": 13,
+        "title": "Defect-induced visible-light-driven photocatalytic and photoelectrochemical performance of ZnO–CeO2 nanoheterojunctions",
+        "authors": "Das, Abinash Patra, Moumita Kumar P, Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G",
+        "journal": "Journal of Alloys and Compounds",
+        "year": "2021",
+        "description": "",
+        "doi": "10.1016/j.jallcom.2020.157730"
+      },
+      {
+        "id": 14,
+        "title": "Hierarchical ZnO-TiO2 nanoheterojunction: A strategy driven approach to boost the photocatalytic performance through the synergy of improved surface area and interfacial charge transport",
+        "authors": "Das, Abinash Kumar, P. Mathan Bhagavathiachari, Muthuraaman Nair, Ranjith G.",
+        "journal": "Applied Surface Science",
+        "year": "2020",
+        "description": "",
+        "doi": "10.1016/j.apsusc.2020.147321"
+      },
+      {
+        "id": 15,
+        "title": "Improved performance of dye-sensitized solar cells upon sintering of a PEDOT cathode at various temperatures",
+        "authors": "Peri, Rajagopal Mathan Kumar, P. Muthuraaman, B.",
+        "journal": "RSC Advances",
+        "year": "2020",
+        "description": "",
+        "doi": "10.1039/c9ra09715e"
+      },
+      {
+        "id": 16,
+        "title": "Role of copper and silver modified titania photoanode on performance engineering of dye sensitized solar cells",
+        "authors": "Nair, Ranjith G. Ojah, Swapna Kumar, P. Mathan Nikhil, S. K. Samdarshi, S. K",
+        "journal": "Materials Letters",
+        "year": "2018",
+        "description": "",
+        "doi": "10.1016/j.matlet.2018.03.150"
+      },
+      {
+        "id": 17,
+        "title": "Enhancing the Photoanode Performance of Dye‐Sensitized Solar Cells through Integration of Cu Doping and Biphasic Heterojunction in TiO2",
+        "authors": "Nikhil SK, Mathan Kumar P, Muthuraaman Bhagavathiachari, Ranjith G. Nair",
+        "journal": "physica status solidi (a",
+        "year": "2025",
+        "description": "",
+        "doi": "10.1002/pssa.202500219"
+      }
+    ],
+    "conference": [
+      {
+        "id": 1,
+        "title": "Performance engineering of dye sensitized solar cells (DSSC) using Ag modified titania as photoanode",
+        "authors": "Ranjith G Nair, P Mathan Kumar, SK Samdarsh",
+        "journal": "IOP Conference Series: Materials Science and Engineering",
+        "year": "2018",
+        "description": "",
+        "doi": "10.1088/1757-899X/303/1/012001"
+      }
+    ],
+    "chapter": [
+      {
+        "id": 1,
+        "title": "Fabrication and life time of perovskite solar cells",
+        "authors": "Mathan Kumar, P. Das, Abinash Seban, Lalu Nair, Ranjith G.",
+        "journal": "Perovskite Photovoltaics: Basic to Advanced Concepts and Implementation",
+        "year": "2018",
+        "description": "",
+        "doi": "10.1016/B978-0-12-812915-9.00008-3"
+      },
+      {
+        "id": 2,
+        "title": "Counter Electrodes for Dye Sensitized Solar Cells (DSSC): An Insight",
+        "authors": "P, Mathan Kumar Peri, Rajagopal T, Mohana Selvi V, Mareeswaran S, Brindha B, Muthuraaman",
+        "journal": "Reference Module in Earth Systems and Environmental Sciences",
+        "year": "2023",
+        "description": "",
+        "doi": "10.1016/b978-0-323-93940-9.00113-4"
+      },
+      {
+        "id": 3,
+        "title": "Bio-Based Polymer Electrolytes for Electrochemical Applications - An Overview",
+        "authors": "Mohana Selvi, T. Brindha, S. Mathan Kumar, P. Mareeswaran, V Muthuraaman, B. Austin Suthanthiraraj, S.",
+        "journal": "Encyclopedia of Renewable Energy, Sustainability and the Environment",
+        "year": "2024",
+        "description": "",
+        "doi": "10.1016/b978-0-323-93940-9.00242-5"
+      }
+    ],
     "patent": []
   },
   "researchCircle": [
